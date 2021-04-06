6859912ce081fc9845bdd7fccfbc3bb9c95d8dc9
Dénes Solti <sodnaatx@gmail.com>
2021-04-06 10:18:58 +0200

    custom roles III

--- SRC/Modules/UserManager.API/IUserManager.cs
+++ SRC/Modules/UserManager.API/IUserManager.cs
@@ -11,16 +11,16 @@ namespace Modules.API
     [ParameterValidatorAspect, RoleValidatorAspect, TransactionAspect]
     public interface IUserManager
     {
-        [RequiredRoles(Roles.AuthenticatedUser), Transactional]
+        [RequiredRoles(Roles.Admin), Transactional]
         Task<Guid> Create([NotNull, ValidateProperties] User user, [NotNull, LengthBetween(min: 5)] string pw);
 
-        [RequiredRoles(Roles.AuthenticatedUser), Transactional]
+        [RequiredRoles(Roles.Admin), Transactional]
         Task Delete(Guid userId);
 
         [RequiredRoles(Roles.AuthenticatedUser), Transactional]
         Task DeleteCurrent();
 
-        [RequiredRoles(Roles.AuthenticatedUser), Transactional(IsolationLevel = IsolationLevel.Serializable)]
+        [RequiredRoles(Roles.Admin), Transactional(IsolationLevel = IsolationLevel.Serializable)]
         Task<PartialUserList> List(int skip, int count);
 
         [RequiredRoles(Roles.AnonymousUser)]
