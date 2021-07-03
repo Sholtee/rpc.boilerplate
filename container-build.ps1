ab80a2171e36f49c8acb313dcf9791d670820a9a
Dénes Solti <sodnaatx@gmail.com>
2021-07-03 16:46:56 +0200

    Docker support V [mounted MySQL data]

--- container-build.ps1
+++ container-build.ps1
@@ -1,5 +1,11 @@
 docker build -t myapp . --progress=plain --build-arg MYSQL_PW=Alma1234 --build-arg CERT_PATH=./cert.self-signed/certificate.p12 --build-arg CERT_PW=cica --build-arg ROOT_PW=Password1
+
 if (!(Test-Path BIN)) {
   New-Item -Path BIN -Force -ItemType "Directory" | Out-Null
 }
+
+$container_id=docker create myapp
+docker cp "$($container_id):/mysql-8.0.25-winx64/data/" "./BIN/mysql-data"
+docker rm $container_id
+
 docker save -o ./BIN/myapp.tar myapp
