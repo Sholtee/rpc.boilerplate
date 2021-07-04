68d32ce9ef79a157261dede5e4f8e759e2add681
Dénes Solti <sodnaatx@gmail.com>
2021-07-04 06:41:12 +0200

    container-build.ps1: empty the output directory before build

--- container-build.ps1
+++ container-build.ps1
@@ -1,8 +1,9 @@
 docker build -t myapp . --progress=plain --build-arg MYSQL_PW=Alma1234 --build-arg CERT_PATH=./cert.self-signed/certificate.p12 --build-arg CERT_PW=cica --build-arg ROOT_PW=Password1 --build-arg CUSTOM_SQL="CREATE USER 'root'@'DENES-PC' IDENTIFIED BY 'cica';GRANT ALL ON *.* TO 'root'@'DENES-PC';"
 
-if (!(Test-Path BIN)) {
-  New-Item -Path BIN -Force -ItemType "Directory" | Out-Null
+if (Test-Path BIN) {
+  Remove-Item BIN -Recurse -Force
 }
+New-Item -Path BIN -Force -ItemType "Directory" | Out-Null
 
 $container_id=docker create myapp
 docker cp "$($container_id):/mysql-8.0.25-winx64/data/" "./BIN/mysql-data"
