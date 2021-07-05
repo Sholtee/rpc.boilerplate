496ea28e2e8845fae6587f3fc2f92d44044c01a7
Dénes Solti <sodnaatx@gmail.com>
2021-07-05 06:47:27 +0200

    build script cosmetics

--- container-build.ps1
+++ container-build.ps1
@@ -1,12 +1,36 @@
-docker build -t myapp . --progress=plain --build-arg MYSQL_PW=Alma1234 --build-arg CERT_PATH=./cert.self-signed/certificate.p12 --build-arg CERT_PW=cica --build-arg ROOT_PW=Password1 --build-arg CUSTOM_SQL="CREATE USER 'root'@'DENES-PC' IDENTIFIED BY 'cica';GRANT ALL ON *.* TO 'root'@'DENES-PC';"
+param(
+  [Parameter(Mandatory=$true)]
+  [string]
+  $MYSQL_PW,
 
-if (Test-Path BIN) {
-  Remove-Item BIN -Recurse -Force
+  [ValidatePattern('^[A-Za-z0-1 .\\/]+\.p12$')]
+  [string]
+  $CERT_PATH='./cert.self-signed/certificate.p12',
+
+  [string]
+  $CERT_PW='cica',
+
+  [Parameter(Mandatory=$true)]
+  [string]
+  $ROOT_PW,
+
+  [Parameter(Mandatory=$true)]
+  [string]
+  $HOST_NAME,
+
+  [string]
+  $BIN_FOLDER='./BIN'
+)
+
+docker build -t myapp . --progress=plain --build-arg MYSQL_PW=$MYSQL_PW --build-arg CERT_PATH=$CERT_PATH --build-arg CERT_PW=$CERT_PW --build-arg ROOT_PW=$ROOT_PW --build-arg CUSTOM_SQL="CREATE USER 'root'@'$($HOST_NAME)' IDENTIFIED BY '$($MYSQL_PW)';GRANT ALL ON *.* TO 'root'@'$($HOST_NAME)';"
+
+if (Test-Path $BIN_FOLDER) {
+  Remove-Item $BIN_FOLDER -Recurse -Force
 }
-New-Item -Path BIN -Force -ItemType "Directory" | Out-Null
+New-Item -Path $BIN_FOLDER -Force -ItemType "Directory" | Out-Null
 
 $container_id=docker create myapp
-docker cp "$($container_id):/mysql-8.0.25-winx64/data/" "./BIN/mysql-data"
+docker cp "$($container_id):/mysql-8.0.25-winx64/data/" "$($BIN_FOLDER)/mysql-data"
 docker rm $container_id | Out-Null
 
-docker save -o ./BIN/myapp.tar myapp
+docker save -o "$($BIN_FOLDER)/myapp.tar" myapp
--- container-run.ps1
+++ container-run.ps1
@@ -1,2 +1,10 @@
+param(
+  [int]
+  $MYSQL_PORT=13306,
+
+  [string]
+  $BIN_FOLDER='./BIN'
+)
+
 docker load -i BIN/myapp.tar
-docker run -it -p 13306:3306 --mount type=bind,source="$(Resolve-Path ./BIN/mysql-data)",target="c:/mysql-8.0.25-winx64/data/" --rm myapp
+docker run -it -p "$($MYSQL_PORT):3306" --mount "type=bind,source=$(Resolve-Path $BIN_FOLDER/mysql-data),target=c:/mysql-8.0.25-winx64/data/" --rm myapp
