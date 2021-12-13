35b6636ea0d095cd4c6ffe44a078d7839c8cab58
Dénes Solti <sodnaatx@gmail.com>
2021-12-13 20:07:43 +0100

    test.ps1: set ErrorActionPreference

--- test.ps1
+++ test.ps1
@@ -1,2 +1,5 @@
+$ErrorActionPreference = 'Stop';
+$ProgressPreference = 'SilentlyContinue';
+
 Start-Process docker -ArgumentList 'compose run --rm test_env pwsh -command ./BUILD/test/test.ps1' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
 Start-Process docker -ArgumentList 'compose down' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
