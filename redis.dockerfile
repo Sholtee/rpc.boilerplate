d2a59f5350d1f69d7c041bfbc920fca12db53694
Dénes Solti <sodnaatx@gmail.com>
2021-12-13 10:34:26 +0100

    containerized tests II

---
+++ redis.dockerfile
@@ -0,0 +1,16 @@
+# prep
+FROM mcr.microsoft.com/powershell:lts-windowsservercore-2004 as prep
+SHELL ["powershell", "-Command", "$ErrorActionPreference = 'Stop'; $ProgressPreference = 'SilentlyContinue';"]
+
+WORKDIR /download
+RUN \
+  Invoke-WebRequest -Method Get -Uri 'https://github.com/MicrosoftArchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.zip' -OutFile 'redis.zip';\
+  Expand-Archive -Path 'redis.zip' -DestinationPath 'redis-bin/';
+
+# app
+FROM mcr.microsoft.com/windows/nanoserver:ltsc2022
+
+WORKDIR /redis
+COPY --from=prep /download/redis-bin .
+
+ENTRYPOINT redis-server.exe
--- test.ps1
+++ test.ps1
@@ -2,7 +2,10 @@ param ($logfolder="Artifacts")
 if (test-path("BIN")) {
   remove-item "BIN" -recurse
 }
+get-childitem -filter "*.sln" -recurse | foreach { 
+  dotnet build $_.fullname --configuration:Debug
+}
 get-childitem -filter "*.Tests.csproj" -recurse | foreach { 
   $logfile=[System.IO.Path]::ChangeExtension($_.name, 'xml')
-  dotnet test $_.fullname --configuration:Debug --test-adapter-path:. --logger:"nunit;LogFilePath=$([System.IO.Path]::Combine(('.\' | resolve-path), $logfolder, $logfile))"
+  dotnet test $_.fullname --no-build --test-adapter-path:. --logger:"nunit;LogFilePath=$([System.IO.Path]::Combine(('.\' | resolve-path), $logfolder, $logfile))"
 }
