edca72c9bd1693778c7c81e9bacfd0f293a82d4c
Dénes Solti <sodnaatx@gmail.com>
2021-12-13 19:16:29 +0100

    containerized tests IV

--- test.cmd
+++
@@ -1,3 +0,0 @@
-docker compose -f ./BUILD/docker-compose-test.yml run --rm test_env pwsh -command ./BUILD/test.ps1
-docker compose -f ./BUILD/docker-compose-test.yml down
-docker container prune --force
---
+++ test.ps1
@@ -0,0 +1,2 @@
+Start-Process docker -ArgumentList 'compose run --rm test_env pwsh -command ./BUILD/test/test.ps1' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
+Start-Process docker -ArgumentList 'compose down' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
