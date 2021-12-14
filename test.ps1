baa3f023b5e09943d69267b74496d99f4186c802
Dénes Solti <sodnaatx@gmail.com>
2021-12-14 19:15:10 +0100

    test.ps1: remove param

--- test.ps1
+++ test.ps1
@@ -1,7 +1,5 @@
-param([switch]$noNewWindow)
-
 $ErrorActionPreference = 'Stop';
 
-$proc = Start-Process docker -ArgumentList 'compose run test_env pwsh -command ./BUILD/test/test.ps1' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow:$noNewWindow -PassThru;
+$proc = Start-Process docker -ArgumentList 'compose run test_env pwsh -command ./BUILD/test/test.ps1' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow -PassThru;
 Write-Host "Docker returned $($proc.ExitCode)";
-Start-Process docker -ArgumentList 'compose down' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow:$noNewWindow;
+Start-Process docker -ArgumentList 'compose down' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
