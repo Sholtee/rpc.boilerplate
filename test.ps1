bd7a48841b3ef514f9d9ddb7982638caceb945b5
Dénes Solti <sodnaatx@gmail.com>
2021-12-14 18:41:58 +0100

    test.ps1: debug

--- test.ps1
+++ test.ps1
@@ -1,5 +1,7 @@
-$ErrorActionPreference = 'Stop';
-$ProgressPreference = 'SilentlyContinue';
+param([switch]$noNewWindow)
 
-Start-Process docker -ArgumentList 'compose run --rm test_env pwsh -command ./BUILD/test/test.ps1' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
-Start-Process docker -ArgumentList 'compose down' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow;
+$ErrorActionPreference = 'Stop';
+
+$proc = Start-Process docker -ArgumentList 'compose run --rm test_env pwsh -command ./BUILD/test/test.ps1' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow:$noNewWindow -PassThru;
+Write-Host "Docker returned $($proc.ExitCode)";
+Start-Process docker -ArgumentList 'compose down' -WorkingDirectory './BUILD/test' -Wait -NoNewWindow:$noNewWindow;
