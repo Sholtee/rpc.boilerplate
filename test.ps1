25950f1c6750342c0c99ed57f960d15cfc47c4b2
Dénes Solti <sodnaatx@gmail.com>
2021-12-19 17:43:03 +0100

    appveyor.yml: dump test log

--- test.ps1
+++ test.ps1
@@ -1,9 +1,12 @@
-param([Parameter(Position = 0, Mandatory = $false)][string]$target)
+param([Parameter(Position = 0, Mandatory = $false)][string]$target, [Parameter(Mandatory = $false)][string]$logFile)
 $ErrorActionPreference = 'Stop';
 
 $remoteCommand='./BUILD/test/test.ps1';
 if ($target -ne '') {
-    $remoteCommand+=" '$target'"
+  $remoteCommand+=" '$target'"
+}
+if ($logFile -ne '') {
+  $remoteCommand+=" | Out-File -path (New-Item -path '$logFile' -force)"
 }
 
 $proc = Start-Process docker-compose -ArgumentList "run test_env pwsh -command $remoteCommand" -WorkingDirectory './BUILD/test' -Wait -NoNewWindow -PassThru;
