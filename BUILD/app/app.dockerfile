6ca231fe9e21b7e6c3dd2b90ccbdc57c16cd6bdf
Dénes Solti <sodnaatx@gmail.com>
2021-12-20 11:09:47 +0100

    app container II

--- BUILD/app/app.dockerfile
+++ BUILD/app/app.dockerfile
@@ -12,14 +12,14 @@ RUN dotnet publish ./SRC/MyApp.sln -c %CONFIGURATION%
 FROM mcr.microsoft.com/dotnet/runtime:5.0-windowsservercore-ltsc2019
 SHELL ["powershell", "-Command", "$ErrorActionPreference = 'Stop'; $ProgressPreference = 'SilentlyContinue';"]
 
-ARG \
+ARG\
   CERT_PATH\
   CERT_PWD
 
 WORKDIR /app
 COPY $CERT_PATH ./certificate.p12
 
-RUN \
+RUN\
   Write-Host 'Registering certificate...';\
   $hash=(Import-PfxCertificate -FilePath './certificate.p12' Cert:\LocalMachine\My -Password (ConvertTo-SecureString -String $ENV:CERT_PWD -Force -AsPlainText)).Thumbprint;\
   $appId=(New-Guid).ToString('B');\
@@ -29,6 +29,10 @@ RUN \
 COPY --from=build app/BIN/net5.0/publish ./Bin
 COPY --from=build app/Migration ./Migration
 COPY --from=build app/BUILD/app/run.ps1 .
+
+ENV\
+  APP_ROOT=root@root.hu\
+  APP_PWD=secret
 
 ENTRYPOINT ./run.ps1
 
--- BUILD/app/run.ps1
+++ BUILD/app/run.ps1
@@ -1,12 +1,12 @@
 function invoke-server([Parameter(Position = 0, Mandatory = $false)][string]$argList) {
-  $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-  $pinfo.FileName = "./BIN/MyApp.Server.exe"
-  $pinfo.RedirectStandardError = $true
-  $pinfo.RedirectStandardOutput = $true
-  $pinfo.UseShellExecute = $false
-  $pinfo.Arguments = $argList
-  $p = New-Object System.Diagnostics.Process
-  $p.StartInfo = $pinfo
+  $pinfo=New-Object System.Diagnostics.ProcessStartInfo
+  $pinfo.FileName="./BIN/MyApp.Server.exe"
+  $pinfo.RedirectStandardError=$true
+  $pinfo.RedirectStandardOutput=$true
+  $pinfo.UseShellExecute=$false
+  $pinfo.Arguments=$argList
+  $p=New-Object System.Diagnostics.Process
+  $p.StartInfo=$pinfo
   $p.Start() | Out-Null
   $p.WaitForExit()
 
@@ -21,7 +21,7 @@ $status=invoke-server "status"
 
 if ($status.StartsWith("NOT INSTALLED")) {
   Write-Host "Installing the app..."
-  invoke-server "install -User $($Env:APP_ROOT) -PasswordVariable APP_PWD"
+  invoke-server "install -User '$($Env:APP_ROOT)' -PasswordVariable APP_PWD"
 } elseif ($status.StartsWith("INSTALLED")) {
   Write-Host "Running the migration scripts..."
   invoke-server "migrate"
