0c836a3241b4613d5d859a9b44f697322f41ec76
Dénes Solti <sodnaatx@gmail.com>
2022-01-30 14:24:24 +0100

    run.ps1: workaround bug introduced in docker engine v20.10.12

--- run.ps1
+++ run.ps1
@@ -4,4 +4,9 @@ $wd='./BUILD/app'
 if ($forceRebuild) {
   Start-Process docker-compose -ArgumentList "build --force-rm app" -WorkingDirectory $wd -Wait -NoNewWindow
 }
-Start-Process docker-compose -ArgumentList "up --remove-orphans" -WorkingDirectory $wd -Wait -NoNewWindow
+
+$proc = Start-Process docker-compose -ArgumentList "up --remove-orphans" -WorkingDirectory $wd -Wait -NoNewWindow -PassThru
+if ($proc.ExitCode -ne 0) {
+  # Engine v20.10.12 workaround
+  Start-Process docker -ArgumentList "compose up --remove-orphans" -WorkingDirectory $wd -Wait -NoNewWindow
+}
